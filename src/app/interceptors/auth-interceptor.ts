e753838f53cd00450be480ee0ce5eaac886e1fd6
Chad <chad_a_collins@phpthis.com>
2025-06-26 20:14:10 +0000

    added http interceptor to inject jwt token in the header

---
+++ src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('jwt');
+
+  //console.log('Interceptor Token:', token);
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(cloned);
+  }
+  return next(req);
+};
